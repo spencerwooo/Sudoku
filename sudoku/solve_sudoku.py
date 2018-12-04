caa3017ea9c87326a85eb2b705e70aacc3258ed0
spencerwooo <spencerwoo98@foxmail.com>
2018-12-04 23:20:39 +0800

    😎 Trying to solve that sudoku!

--- sudoku/solve_sudoku.py
+++ sudoku/solve_sudoku.py
@@ -5,13 +5,66 @@ import numpy
 
 
 class SolveSudoku():
+    # An ignorant initial blank baby sudoku
+    solveThatSudoku = []
+
     def __init__(self, sudokuPath):
         try:
-            solveThatSudoku = numpy.loadtxt(sudokuPath, dtype=int)
+            self.solveThatSudoku = numpy.loadtxt(sudokuPath, dtype=int)
             print("[INFO] Begin solving sudoku at", sudokuPath, "...")
 
-            print(solveThatSudoku)
+            self.solvingSudoku(self.solveThatSudoku)
 
-        except Exception:
+            print(self.solveThatSudoku)
+
+        except FileNotFoundError:
             print("[ERR] File not found.")
 
+    def solvingSudoku(self, sudoku):
+        currentBlock = [0, 0]
+
+        if not self.blockIsEmpty(currentBlock):
+            return True
+        row = currentBlock[0]
+        col = currentBlock[1]
+
+        for num in range(1, 9):
+            if self.numIsCandidate(row, col, num):
+                self.solveThatSudoku[row][col] = num
+                if self.solvingSudoku(self.solveThatSudoku):
+                    return True
+                self.solveThatSudoku[row][col] = 0
+        
+        return False
+
+    def blockIsEmpty(self, currentBlock):
+        for i in range(9):
+            for j in range(9):
+                if self.solveThatSudoku[i][j] == 0:
+                    currentBlock[0] = i
+                    currentBlock[1] = j
+                    # print("empty: row="+str(i)+" col="+str(j))
+                    return True
+        return False
+
+    def numUsedInRow(self, currentRow, currentNum):
+        for i in range(9):
+            if self.solveThatSudoku[currentRow][i] == currentNum:
+                return True
+        return False
+
+    def numUsedInCol(self, currentCol, currentNum):
+        for i in range(9):
+            if self.solveThatSudoku[i][currentCol] == currentNum:
+                return True
+        return False
+
+    def numUsedInBigBlock(self, currentRow, currentCol, currentNum):
+        for i in range(3):
+            for j in range(3):
+                if self.solveThatSudoku[currentRow + i][currentCol + j] == currentNum:
+                    return True
+        return False
+
+    def numIsCandidate(self, row, col, num):
+        return not self.numUsedInRow(row, num) and not self.numUsedInCol(col, num) and not self.numUsedInBigBlock(row - row % 3, col - col % 3, num)
