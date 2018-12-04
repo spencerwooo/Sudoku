987bad8323499272d32a307de78da05b2248c87a
spencerwooo <spencerwoo98@foxmail.com>
2018-12-04 13:37:46 +0800

    :sparkles: Add solve sudoku tests

--- sudoku/solve_sudoku.py
+++ sudoku/solve_sudoku.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
+import numpy
+
 
 class SolveSudoku():
     def __init__(self, sudokuPath):
-        print("[INFO] Begin solving sudoku at", sudokuPath, "...")
+        try:
+            solveThatSudoku = numpy.loadtxt(sudokuPath, dtype=int)
+            print("[INFO] Begin solving sudoku at", sudokuPath, "...")
+
+            print(solveThatSudoku)
+
+        except Exception:
+            print("[ERR] File not found.")
+
