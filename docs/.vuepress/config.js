4aa7d05ce2f532e7ee0f45aad6b83b436f1b48d6
spencerwooo <spencerwoo98@foxmail.com>
2018-12-05 20:11:42 +0800

    📑 Initialize VuePress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  title: 'Sudoku',
+  description: '命令行交互的基础数独工具库',
+  base: '',
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/favicon.png'
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#00ABE9'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'black'
+    }],
+    ['link', {
+      rel: 'apple-touch-icon',
+      href: '/favicon.png'
+    }],
+    ['meta', {
+      name: 'msapplication-TileImage',
+      content: '/favicon.png'
+    }],
+    ['meta', {
+      name: 'msapplication-TileColor',
+      content: '#06BDFF'
+    }]
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/spencerwooo/Sudoku'
+      },
+    ],
+    lastUpdated: 'Last Updated'
+  }
+}
