b80e6eead5a635d24fc459faf6bf0ffd48432ebd
spencerwooo <spencerwoo98@foxmail.com>
2018-12-09 15:31:16 +0800

    🚀 Dramatically optimize performance

--- sudoku/solve_sudoku.py
+++ sudoku/solve_sudoku.py
@@ -18,58 +18,80 @@ class SolveSudoku():
                 sudoku_answer_file.truncate(0)
                 for each_sudoku in sudoku_list:
                     start_time = time.time()
+
                     self.solving_sudoku(each_sudoku)
                     # print(each_sudoku)
                     numpy.savetxt(sudoku_answer_file, each_sudoku, fmt='%d')
                     sudoku_answer_file.write('\n')
-                    print('[SOLVED!] Time:', round(time.time() - start_time, 4), 'seconds.')
+
+                    print('[SOLVED!] Time:', round(
+                        time.time() - start_time, 4), 'seconds.')
 
         except FileNotFoundError:
             print("[ERR] File not found.")
 
     def solving_sudoku(self, sudoku):
-        current = [0, 0]
+        # 每个格子所在列的数字集合
+        col_num = []
+        # 每个格子所在行的数字集合
+        row_num = []
+        # 每个格子所在 3x3 子块的数字集合
+        sqr_num = [[0]*3 for _ in range(3)]
 
-        if not self.find_that_blank(sudoku, current):
+        # 初始化
+        for i in range(9):
+            col_num.append(set(tuple(range(1, 10))))
+            row_num.append(set(tuple(range(1, 10))))
+
+        for i in range(3):
+            for j in range(3):
+                sqr_num[i][j] = set(tuple(range(1, 10)))
+
+        blank_in_sudoku = set()
+
+        for i in range(9):
+            for j in range(9):
+                if sudoku[i][j] != 0:
+                    if sudoku[i][j] in row_num[i]:
+                        row_num[i].remove(sudoku[i][j])
+                    if sudoku[i][j] in col_num[j]:
+                        col_num[j].remove(sudoku[i][j])
+                    if sudoku[i][j] in sqr_num[i//3][j//3]:
+                        sqr_num[i//3][j//3].remove(sudoku[i][j])
+                else:
+                    blank_in_sudoku.add((i, j))
+
+        self.dfs(blank_in_sudoku, col_num, row_num, sqr_num, sudoku)
+
+    def dfs(self, blank_in_sudoku, col_num, row_num, sqr_num, sudoku):
+        if not blank_in_sudoku:
             return True
-        row = current[0]
-        col = current[1]
 
-        for num in range(1, 10):
-            if self.num_is_candidate(sudoku, num, row, col):
-                sudoku[row][col] = num
-                if self.solving_sudoku(sudoku):
-                    return True
-                sudoku[row][col] = 0
-        return False
+        max_possible_len = 9
 
-    def num_is_candidate(self, sudoku, num, row, col):
-        # check if num is used in row
-        for i in range(9):
-            if sudoku[row][i] == num:
-                return False
+        for (m, n) in blank_in_sudoku:
+            candidate_num = row_num[m] & col_num[n] & sqr_num[m//3][n//3]
+            if len(candidate_num) <= max_possible_len:
+                max_possible_len = len(candidate_num)
+                i, j = m, n
 
-        # check if num is used in column:
-        for i in range(9):
-            if sudoku[i][col] == num:
-                return False
+        if max_possible_len == 0:
+            return False
 
-        # check if num is used in block:
-        row_start = int(row / 3) * 3
-        col_start = int(col / 3) * 3
-        # print(row_start, col_start)
-        for i in range(row_start, row_start + 3):
-            for j in range(col_start, col_start + 3):
-                if sudoku[i][j] == num:
-                    return False
+        blank_in_sudoku.remove((i, j))
+        candidate_num = row_num[i] & col_num[j] & sqr_num[i//3][j//3]
 
-        return True
+        for num in candidate_num:
+            row_num[i].remove(num)
+            col_num[j].remove(num)
+            sqr_num[i//3][j//3].remove(num)
+            sudoku[i][j] = num
+            if self.dfs(blank_in_sudoku, col_num, row_num, sqr_num, sudoku):
+                return True
+            sudoku[i][j] = 0
+            row_num[i].add(num)
+            col_num[j].add(num)
+            sqr_num[i//3][j//3].add(num)
 
-    def find_that_blank(self, sudoku, current):
-        for row in range(9):
-            for col in range(9):
-                if sudoku[row][col] == 0:
-                    current[0] = row
-                    current[1] = col
-                    return True
+        blank_in_sudoku.add((i, j))
         return False
